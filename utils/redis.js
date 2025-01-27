1e38c796bf5c6ffc54d472551c992994cb1c3d85
gabriel <gabaamazing21@gmail.com>
2025-01-27 10:07:12 +0100

    rewrite redisclient configuration

--- utils/redis.js
+++ utils/redis.js
@@ -1,37 +1,53 @@
 import { createClient } from 'redis';
-
-const client = createClient();
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     // create a redis client
+    const client = createClient();
     this.client = client;
     // conncect to redis
     this.client.on('error', (err) => console.log('Redis Client Error', err));
-    this.client.on('connect', () => {
-      console.log('Connected to Redis server');
-    });
+    // this.client.on('connect', () => {
+    //   console.log('Connected to Redis server');
+    // });
+    this._getAsync = promisify(this.client.get).bind(this.client);
+    this._setAsync = promisify(this.client.set).bind(this.client);
+    this._delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
     return this.client.connected;
   }
 
-  async set(key, value) {
-    await this.client.set(key, value);
-    console.log(`set key ${key}, value: ${value}`);
+  async set(key, value, duration) {
+    try {
+      await this._setAsync(key, value, 'EX', duration);
+    } catch (error) {
+      console.error('Error setting key: ', error);
+    }
   }
 
   async get(key) {
-    const value = await this.client.get(key);
-    console.log(`Got value for key ${key}: ${value}`);
+    try {
+      const value = await this._getAsync(key);
+      console.log(`Got value for key ${key}: ${value}`);
+      return value;
+    } catch (error) {
+      console.error('Error getting key: ', error);
+      return null;
+    }
   }
 
   async del(key) {
-    await this.client.del(key);
-    console.log(`Deleted key: ${key}`);
+    try {
+      await this._delAsync(key);
+      console.log(`Deleted key: ${key}`);
+    } catch (error) {
+      console.error('Error deleting key: ', error);
+    }
   }
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
