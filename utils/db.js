3a824dadb69b314369d947a069e1795072e95ec8
gabriel <gabaamazing21@gmail.com>
2025-01-27 14:25:20 +0100

    okay

--- utils/db.js
+++ utils/db.js
@@ -1,46 +1,43 @@
 const { MongoClient } = require('mongodb');
 
+class DBClient {
+  constructor() {
+    const dbName = 'files_manager';
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || dbName;
 
-class DBClient{
-    constructor (){
-        
-    
-    const db_name = 'files_manager';
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.dbName = database;
+    this.client.connect()
+      .then(() => console.log('Connected to MongoDB'))
+      .catch((err) => console.error('MongoDB connection error:', err));
+  }
 
-        const url = `mongodb://${host}:${port}`;
-        this.client = new MongoClient(url, {useUnifiedTopology: true});
-        this.dbName = database;
-        this.client.connect()
-        .then(()=> console.log('Connected to MongoDB'))
-        .catch((err) => console.error('MongoDB connection error:', err));
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    try {
+      const db = this.client.db(this.dbName);
+      return await db.collection('users').countDocuments();
+    } catch (err) {
+      console.error('Error fetching user count:', err);
+      return 0;
     }
+  }
 
-    isAlive(){
-        return this.client.isConnected();
+  async nbFiles() {
+    try {
+      const db = this.client.db(this.dbName);
+      return await db.collection('files').countDocuments();
+    } catch (err) {
+      console.log('Error fetching file count:', err);
+      return 0;
     }
-
-    async nbUsers(){
-        try{
-            const db = this.client.db(this.dbName);
-            return await db.collection('users').countDocuments();
-        } catch (err) {
-            console.error('Error fetching user count:', err);
-            return 0;
-        }
-    }
-
-    async nbFiles(){
-        try{
-            const db = this.client.db(this.dbName);
-            return await db.collection('files').countDocuments();
-        } catch(err){
-            console.log('Error fetching file count:', err);
-            return 0;
-        }
-    }
+  }
 }
 
 // create and export an instance of a class
