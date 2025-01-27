9fac5a920a9336f0083c04bfe121d2875f08aba5
gabriel <gabaamazing21@gmail.com>
2025-01-28 00:34:38 +0100

    okay

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import AppController from '../controller/AppController';
-import UserController from '../controller/UsersController';
-import AuthController from '../controller/AuthController';
+import AppController from '../controllers/AppController';
+import UserController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
