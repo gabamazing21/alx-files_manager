1aedd9fb2bcc17c65c5ea910e883473a11222237
gabriel <gabaamazing21@gmail.com>
2025-01-28 10:13:18 +0100

    okay

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import router from './routes/index';
 import redisClient from './utils/redis';
+import dbClient from './utils/db';
 
 const app = express();
+const dbALive = dbClient.isAlive();
+console.log(dbALive);
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
