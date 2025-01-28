708e0a2afdde8f054cb9f8fac0ea18e5643b50bd
gabriel <gabaamazing21@gmail.com>
2025-01-28 09:28:13 +0100

    fix getstat

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ import AuthController from '../controllers/AuthController';
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 router.post('/users', UserController.postNew);
 router.get('/users/me', UserController.getMe);
 router.get('/connect', AuthController.getConnect);
